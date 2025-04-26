84978abdc432361a292aa644c3bb3ce39c5e9ac1
AnthonyGrullonA <anthonygrulon283@gmail.com>
2025-04-26 00:25:13 -0400

    Fix error urls

--- demo/urls.py
+++ demo/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.url0asdasds),
-    path('api/', include('api.urlsasdasdasd'))
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls'))
 ]
